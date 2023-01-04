cf2924ee5733de1df2214c03d8dde6cf88f4b506
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-01-04 17:50:01 +0000

    Added __str__

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+del element, lista
+
+
 class element:
     def __init__(self, pDane, pNastepny=None):
         self.dane = pDane
@@ -13,40 +16,51 @@ class lista:
         self.ogon = None
         self.dlugosc = 0
         
+    
     def __repr__(self):
         return f'{self.__dict__}'
     
+    
+    def __str__(self):
+        lista_wyj = "["
+        tmp_adres = self.glowa
+        
+        while tmp_adres is not None:
+            lista_wyj += str(tmp_adres.dane)            
+
+            tmp_adres = tmp_adres.nastepny
+            if tmp_adres is not None: #dodanie przecinka i spacji, ale nie dla osatniego elementu
+                lista_wyj += ", "
+                      
+        lista_wyj += "]"
+        return lista_wyj    
+    
+    
     def szukaj(self, pElement):
         tmp_nastepny = self.glowa
-        tmp_dane = tmp_nastepny.dane
                 
         while tmp_nastepny is not None:
             tmp_dane = tmp_nastepny.dane
 
             if tmp_dane == pElement:
-                print(f'Znaleziono {pElement}!')
-                return
+                print (f'Znaleziono {pElement}!')
+                return True
                 
             tmp_nastepny = tmp_nastepny.nastepny
             
-        print(f'Nie znaleziono {pElement} wśród elementów podanej listy.')
-
-
-# Wstawienie i testy
-lst = lista()
-q = element(3)
-lst.glowa = q
-lst.ogon = q
-
-r = element(5)
-q.nastepny = r
-lst.ogon = r
-
-
-s = element(7)
-r.nastepny = s
-lst.ogon = s
-
-print(lst)
-lst.szukaj(10)
-lst.szukaj(3)
+        if tmp_nastepny is None:
+            print(f'Nie znaleziono {pElement} wśród elementów podanej listy.') 
+            return False 
+            
+    
+    def dolacz(self, pNowyElement):
+        nowy_element = element(pNowyElement)
+        
+        if self.glowa is None: #pusta lista
+            self.glowa = nowy_element
+            self.ogon = nowy_element
+        else:
+            self.ogon.nastepny = nowy_element
+            self.ogon = nowy_element
+            
+        self.dlugosc += 1
